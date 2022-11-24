2acb322874fd6d708268952fd984ea107d47e4c4
Fabien Fernandes Alves <fabien.fernandes@prestashop.com>
2022-11-24 16:18:30 +0100

    WIP : Try to implement zoid

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import { WidgetEditionZoid } from "./components/WidgetEdition.zoid";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
