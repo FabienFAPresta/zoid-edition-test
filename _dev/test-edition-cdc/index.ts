506d3d7aa7c49bd5a5503a9690d3160257e96503
Fabien Fernandes Alves <fabien.fernandes@prestashop.com>
2022-11-25 09:31:46 +0100

    Added : Try another folder to store cdc

--- _dev/test-edition-cdc/index.ts
+++ _dev/test-edition-cdc/index.ts
@@ -1,6 +1,17 @@
 import { WidgetEditionZoid } from "./components";
 
 export function test() {
-  console.log("Bundle zoid-edition-test loaded");
+  console.log("test");
+}
+
+export function sayHello() {
+  console.log("hello");
+}
+
+export function getWidgetEdition() {
   return WidgetEditionZoid;
 }
+
+console.log("Bundle zoid-edition-test loaded");
+
+export { WidgetEditionZoid };
