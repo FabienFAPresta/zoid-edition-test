899c1078d0f6df9876a6d666f5f0e03542ff5de5
Fabien Fernandes Alves <fabien.fernandes@prestashop.com>
2022-11-25 09:09:08 +0100

    Added : Try another folder to store cdc

--- _dev/test-edition-cdc/components/WidgetEdition.zoid.ts
+++ _dev/test-edition-cdc/components/WidgetEdition.zoid.ts
@@ -1,8 +1,12 @@
 import * as zoid from "zoid/dist/zoid.frameworks";
 
-export const WidgetEditionZoid = zoid.create({
+export var WidgetEditionZoid = zoid.create({
   tag: "widget-edition",
   url: "http://localhost:5173",
+  dimensions: {
+    width: "400px",
+    height: "200px",
+  },
   // props: {
   //   name: {
   //     type: String,
