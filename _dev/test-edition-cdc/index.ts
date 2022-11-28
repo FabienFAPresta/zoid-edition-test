4ceb243e768eeda1eac6f38f885b75273ed549b9
SMB Edition Squad <smbedition@prestashop.com>
2022-11-28 09:34:59 +0100

    Changed : Clean exports

--- _dev/test-edition-cdc/index.ts
+++ _dev/test-edition-cdc/index.ts
@@ -1,17 +1,2 @@
-import { WidgetEditionZoid } from "./components";
-
-export function test() {
-  console.log("test");
-}
-
-export function sayHello() {
-  console.log("hello");
-}
-
-export function getWidgetEdition() {
-  return WidgetEditionZoid;
-}
-
-console.log("Bundle zoid-edition-test loaded");
-
-export { WidgetEditionZoid };
+export { WidgetEditionZoid } from "./components";
+export * as zoid from "zoid/dist/zoid.frameworks";
