8660c323612e306fd8cfecca2b10ab9d17df8bca
SMB Edition Squad <smbedition@prestashop.com>
2022-11-28 16:11:10 +0100

    WIP : Try with hosted component

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,5 @@ import App from "./App.vue";
 import { WidgetEditionZoid } from "test-edition-cdc";
 
 const app = createApp(App);
+app.component("widget-edition-zoid", WidgetEditionZoid.driver("vue3"));
 app.mount("#app");
